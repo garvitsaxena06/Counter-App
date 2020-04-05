2f9104565f9d4abb8477834919c5e52da96e2614
unknown <garvitsaxena06@gmail.com>
2020-04-05 17:09:25 +0530

    Updates

---
+++ precache-manifest.0f6b35e719c577c21c90e1d42f0edb87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f07970d682a14db115df18960fdcdb",
+    "url": "/Counter-App/index.html"
+  },
+  {
+    "revision": "7e3dea42a06417e32f65",
+    "url": "/Counter-App/static/css/main.be1f841f.chunk.css"
+  },
+  {
+    "revision": "1ddf748fb53f598ba407",
+    "url": "/Counter-App/static/js/2.b2821e01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter-App/static/js/2.b2821e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3dea42a06417e32f65",
+    "url": "/Counter-App/static/js/main.09201fb9.chunk.js"
+  },
+  {
+    "revision": "760b2365226981409aa3",
+    "url": "/Counter-App/static/js/runtime-main.2f0243b8.js"
+  },
+  {
+    "revision": "aebb57bcb760f7afa36960fbf9e0f07d",
+    "url": "/Counter-App/static/media/back.aebb57bc.jpg"
+  }
+]);
